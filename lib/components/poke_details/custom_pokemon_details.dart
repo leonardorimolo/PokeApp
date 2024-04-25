22752c9ccf57a901f8f706ec519276e3901b0529
Leonardo Rimolo <leonardorimolo1331@gmail.com>
2024-04-25 20:30:53 -0300

    Ajuste na imagem de erro do pikachu

--- lib/components/poke_details/custom_pokemon_details.dart
+++ lib/components/poke_details/custom_pokemon_details.dart
@@ -34,16 +34,15 @@ class CustomPokeDetails extends StatelessWidget {
                 child: Row(
                   children: [
                     Expanded(
-                      flex: 3,
+                      flex: 2,
                       child: Text(
                         capitalizeFirstLetter(name),
                         style: boldTitleStyleWhite,
                         overflow: TextOverflow.ellipsis,
                       ),
                     ),
-                    Spacer(),
                     Expanded(
-                      flex: 1,
+                      flex: 2,
                       child: Text(
                         ' #$id',
                         style: boldTitleStyleWhite,
@@ -56,7 +55,7 @@ class CustomPokeDetails extends StatelessWidget {
               ),
             ),
             CachedNetworkImage(
-              errorWidget: (context, url, error) => Image.asset('assets/images/pikachu_error_image',height: MediaQuery.sizeOf(context).height*0.35,),
+              errorWidget: (context, url, error) => Image.asset('assets/images/pikachu_error_image.png',height: MediaQuery.sizeOf(context).height*0.35,),
               imageUrl: imageUrl, height: MediaQuery.sizeOf(context).height*0.35, filterQuality: FilterQuality.high,placeholder: (context, url) => const Center(
               child: Padding(
                 padding: EdgeInsets.all(8.0),
